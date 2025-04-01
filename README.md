8a6f3a45913041d1cb7b750f91efe010fe8bb9a1
ZL <ray040406@gmail.com>
2025-04-01 18:21:07 +1300

    route params

--- README.md
+++ README.md
@@ -9,3 +9,28 @@ app.get("/", (request, response) => {
   response.status(201).send("Hello World!");
 });
 ```
+
+## Route parameters
+**dynamic data** based on the route
+```Javascript
+console.log(request.params);
+```
+```Javascript
+app.get("/api/users/:id", (request, response) => {
+  // returns a single user record based on id
+  console.log(request.params);
+  const parsedId = parseInt(request.params.id);
+  console.log(parsedId);
+  if (isNaN(parsedId))
+    return response.status(400).send("Bad request. Invalid id");
+  const findUser = mockUsers.find((user) => user.id == parsedId);
+  if (!findUser) return response.status(404);
+  return response.send(findUser);
+});
+```
+1. ```:id``` is entered after the specified route
+2. get the ```params``` from the response object that contains all the information
+3. parse the ```id``` to a number and check if it is a valid int
+4. if yes, find the user using the id from the database
+5. if not found, return 404
+6. otherwise return user object
--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+  { id: 1, username: "a" },
+  { id: 2, username: "b" },
+];
+
+// localhost:3000
 app.listen(PORT, () => {
   console.log(`Running on Port${PORT}`);
 });
@@ -12,14 +18,30 @@ app.get("/", (request, response) => {
   response.status(201).send("Hello World!");
 });
 
+// localhost:3000/api/users
 app.get("/api/users", (request, response) => {
+  response.send(mockUsers);
+});
+
+// localhost:3000/api/products
+app.get("/api/products", (request, response) => {
   response.send([
-    { id: 1, username: "a" },
-    { id: 2, username: "b" },
+    { id: 1, name: "product_1", price: 10 },
+    { id: 2, name: "product_2", price: 20 },
   ]);
 });
 
-// localhost:3000
-// localhost:3000/users
-// localhost:3000/products
+// route parameter
+app.get("/api/users/:id", (request, response) => {
+  // returns a single user record based on id
+  console.log(request.params);
+  const parsedId = parseInt(request.params.id);
+  console.log(parsedId);
+  if (isNaN(parsedId))
+    return response.status(400).send("Bad request. Invalid id");
+  const findUser = mockUsers.find((user) => user.id == parsedId);
+  if (!findUser) return response.status(404);
+  return response.send(findUser);
+});
+
 // ``````
