51e93d7942e849a27cce436e4be826d9a020d882
zlrkw11 <ray040406@gmail.com>
2025-04-02 17:36:57 +1300

    connect html with express

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,12 @@
 import express from "express";
+import path from "path"
+import { fileURLToPath } from "url";
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const mockUsers = [
   { id: 1, username: "a" },
@@ -15,7 +19,7 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (request, response) => {
-  response.status(201).send("Hello World!");
+  response.sendFile(path.join(__dirname, '..', "home.html"))
 });
 
 // localhost:3000/api/users
