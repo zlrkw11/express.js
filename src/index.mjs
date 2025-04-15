b5e5f80e39de2f99203f09d80f38510ad31d12dc
ZL <ray040406@gmail.com>
2025-04-15 16:20:35 +1200

    feat: query parameters added

--- src/index.mjs
+++ src/index.mjs
@@ -51,7 +51,12 @@ app.get("/", loggingMiddleware, (request, response) => {
 // localhost:3000/api/users
 app.get(
   "/api/users",
-  query("filter").isString().notEmpty(),
+  query("filter")
+    .isString()
+    .notEmpty()
+    .withMessage("Must not be empty")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("Must be at least 3-10 characters"),
   (request, response) => {
     const result = validationResult(request);
     console.log(result);
